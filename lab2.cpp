9cff8e0ed593f7b1bfb3ca8b475bec8012397516
lana <lana.sprajc@fer.hr>
2024-01-14 21:55:42 +0100

    removed unused code

--- lab2.cpp
+++ lab2.cpp
@@ -2,17 +2,14 @@
 #include <GL/glew.h>
 #include <ctime>
 #include <fstream>
-#include "particle.hpp"
+#include <GL/glut.h>
+#include <glm/glm.hpp>
 #define MAXP 20000
 
 GLuint tex2d;
 
-Ociste	ociste = { 0.4f, 0.0f, 1.8f }, glediste = {0.0f, 0.0f, 0.0f};
+glm::vec3 ociste = { 0.4f, 0.0f, 1.8f }, glediste = {0.0f, 0.0f, 0.0f};
 float w, h;
-glm::vec3 wind_dir;
-Particle p[MAXP];
-int intensity = MAX_INTENSITY;
-int time_ = 0;
 GLint utime;
 
 GLuint loadShader(GLenum type)
@@ -56,15 +53,6 @@ void renderScene()
 
 void animate(int value)
 {
-    time_ += 20;
-    if(time_ > 3000) {
-        time_ = 0;
-        intensity--;
-    }
-    for(int i = 0; i <MAXP; i++) {
-        p[i].process(wind_dir);
-        //p[i].process(wind_dir);
-    }
     glutPostRedisplay();
     glutTimerFunc(20, animate, 0);
 }
@@ -91,60 +79,11 @@ void reshape(int width, int height)
 
 void myKeyboard(unsigned char theKey, int mouseX, int mouseY)
 {
-    float len = (ociste-glediste).length();
-    if(theKey == 'a' && ociste.x < 0)
-        theKey = 'd';
-    else if(theKey == 'd' && ociste.x > 0)
-        theKey = 'a';
-    float deltax = 0.0f, deltaz = 0.0f;
     switch (theKey)
     {
-    case 'w':
-        deltax = 0.2f * (glediste.x - ociste.x) / len;
-        deltaz = 0.2f * (glediste.z - ociste.z) / len;
-        break;
-    case 'a':
-        deltaz = 0.2f * (ociste.z - glediste.z) / (ociste.x - glediste.x);
-        deltax = -0.2f;
-        break;
-    case 's':
-        deltax = -0.2f * (glediste.x - ociste.x) / len;
-        deltaz = -0.2f * (glediste.z - ociste.z) / len;
-        break;
-    case 'd':
-        deltaz = -0.2f * (ociste.z - glediste.z) / (ociste.x - glediste.x);
-        deltax = 0.2f;
-        break;
-    case 'r': 
-        ociste = { 0.4f, 0.0f, 1.8f }, glediste = {0.0f, 0.0f, 0.0f};
-        break;
-    case '+':
-        intensity++;
-        if(intensity > 20) intensity = 20;
-        break;
-    case '-':
-        intensity--;
-        if(intensity < 0) intensity = 0;
-        break;
     case 27:  exit(0);
         break;
     }
-    ociste.x += deltax;
-    ociste.z += deltaz;
-    glediste.x += deltax;
-    glediste.z += deltaz;
-    printf("%lf, %lf, %lf\n", ociste.x, ociste.y, ociste.z);
-    reshape(w, h);
-    glutPostRedisplay();
-}
-
-void mouseCallback(int x, int y, int a, int b) {
-    printf("%d, %d, %d, %d\n", x, y, a, b);
-}
-//27
-void motionCallback(int x, int y) {
-    wind_dir.x = ((float)x - w/2) / 2000000.f;
-    wind_dir.y = -((float)y - h/2) / 9000000.f;
 }
 
 int main(int argc, char ** argv)
@@ -159,10 +98,6 @@ int main(int argc, char ** argv)
     glutReshapeFunc(reshape);
     glutTimerFunc(15, animate, 0);
     glutKeyboardFunc(myKeyboard);
-    glutMouseFunc(mouseCallback);
-    glutMotionFunc(motionCallback);
-
-    glutPassiveMotionFunc(motionCallback);
 
     glewInit();
     GLuint vertex = loadShader(GL_VERTEX_SHADER);
@@ -178,14 +113,5 @@ int main(int argc, char ** argv)
     glUseProgram(program);
     utime = glGetUniformLocation(program, "time");
 
-    //tex2d = SOIL_load_OGL_texture
-    //(
-    //    "iskrica.tga",
-    //    SOIL_LOAD_AUTO,
-    //    SOIL_CREATE_NEW_ID,
-    //    SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
-    //);
-    //glBlendEquation(GL_FUNC_ADD);
-    //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glutMainLoop();
 }
